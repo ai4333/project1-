652b4e2d08bd8844db652ad0277198716c8251a7
nikhil sridhara <nikhilsridhara098@gmail.com>
2026-01-09 02:32:07 +0530

    FIX IMAGES: Use relative paths for assets

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,8 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/project1-/" element={<Index />} />
+          <Route path="/project1-" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
